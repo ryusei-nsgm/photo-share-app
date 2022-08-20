2f8181e5157b066751fc6dac4a18fac8be446096
Ryusei <sidapple@gmail.com>
2022-08-20 15:29:44 +0900

    ストアの実装(user state)

--- web/resources/js/store/auth.js
+++ web/resources/js/store/auth.js
@@ -1,10 +1,23 @@
-const state = {}
+const state = {
+  user: null
+}
 
 const getters = {}
 
-const mutations = {}
+const mutations = {
+  // user state を更新する関数
+  setUser (state, user) {
+    state.user = user
+  }
+}
 
-const actions = {}
+const actions = {
+  async register (context, data) {
+    const response = await axios.post('/api/register', data)
+    // commit() => ミューテーションの呼び出し setUserでstate更新
+    context.commit('setUser', response.data)
+  }
+}
 
 export default {
   // namespaced:true モジュールに分けたときにステートやミューテーションの名前が被ってもモジュール名で区別できる
